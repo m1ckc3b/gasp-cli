5973443b2763c2c21fbd02899407b3ee99be442d
Cebulski.mi <michael@automatigo.fr>
2022-10-11 20:00:45 +0200

    add cmd

--- src/help.js
+++ src/help.js
@@ -1,6 +1,6 @@
 
 export default function getHelp() {
-  console.log(
-    `Besoin d'aide ?\nPour créer un nouveau projet, tape :\n\tgasp create [project_name] [scriptId]`
-  );
+  console.log(`Besoin d'aide ?`);
+  console.log("👉 Pour créer un nouveau projet, tape :\n\tgasp create [project_name] [scriptId]");
+  console.log("👉 Pour connaître la version, tape :\n\tgasp version");
 }
